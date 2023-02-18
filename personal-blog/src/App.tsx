eddd60ba112b0fa571bde31bdc36a61696fc65b7
Rodrigo Barrientos <rodrigo.barrientos2310@gmail.com>
2023-02-18 13:00:19 -0500

    added appbar and button

--- personal-blog/src/App.tsx
+++ personal-blog/src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ButtonAppBar from './Appbar';
+import Demo from './Demo';
 
 function App() {
   return (
-    <h1 className="text-2xl font-bold underline">
-      Hello world! From Tailwind
-    </h1>
+    <div>
+      <ButtonAppBar/>
+      <div>
+        <h1 className="text-2xl font-bold underline">
+          Hello world! From Tailwind
+        </h1>
+        <Demo/>
+      </div>
+    </div>
   );
 }
 
---
+++ personal-blog/src/Demo.tsx
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button';
+
+export default function Demo() {
+  return (
+    <div>
+      <Button variant="contained">Good Button</Button>
+    </div>
+  );
+}
