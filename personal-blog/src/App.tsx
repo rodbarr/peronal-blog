1184dad114a8571ebd04bffc5281d9eb7e15c510
Rodrigo Barrientos <rodrigo.barrientos2310@gmail.com>
2023-02-18 01:23:20 -0500

    new beginnings ts + tailwind

---
+++ personal-blog/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-2xl font-bold underline">
+      Hello world! From Tailwind
+    </h1>
+  );
+}
+
+export default App;
